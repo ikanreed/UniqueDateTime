7f430724bd748a5ccc1402bb4dd0f06c7e5e26ae
ikanreed <adrian.cline@gmail.com>
2025-08-04 12:27:24 -0400

    Validate(and test validation) basic structure of string

--- BreakTime/BreakTime/ParseDate.c
+++ BreakTime/BreakTime/ParseDate.c
@@ -1,6 +1,26 @@
 #include "DateTimeStruct.h"
 #include "StringHelper.h"
 
+
+int RequireDigit(char* string, int index)
+{
+	char characterAtIndex = string[index];
+	if (characterAtIndex >= '0' && characterAtIndex <= '9')
+	{
+		return 0;
+	}
+	return 1;
+}
+int RequireValue(char* string, int index, char value)
+{
+	char characterAtIndex = string[index];
+	if (characterAtIndex == value)
+	{
+		return 0;
+	}
+	return 1;
+}
+
 /// <summary>
 /// Checks that the basic format of the string to be parsed is correct
 /// </summary>
@@ -9,8 +29,65 @@
 /// <returns>0 if potentially valid, non-zero if invalid.</returns>
 int validate_datetime_string_format(char* datestring, int str_length)
 {
-	//todo: basic implementation
-	return 1;
+	//There are only two valid post-trimming lengths for datetimes.  Those with a +/- HH:MM and those with just Z at the end
+	if (str_length != 20 && str_length != 25)
+	{
+		return 1;
+	}
+	int invalid = 0;
+	
+	//year
+	invalid |= RequireDigit(datestring, 0);
+	invalid |= RequireDigit(datestring, 1);
+	invalid |= RequireDigit(datestring, 2);
+	invalid |= RequireDigit(datestring, 3);
+
+	invalid |= RequireValue(datestring, 4, '-');
+	//month
+	invalid |= RequireDigit(datestring, 5);
+	invalid |= RequireDigit(datestring, 6);
+
+	invalid |= RequireValue(datestring, 7, '-');
+	//day
+	invalid |= RequireDigit(datestring, 8);
+	invalid |= RequireDigit(datestring, 9);
+
+	invalid |= RequireValue(datestring, 10, 'T');
+	//hour
+	invalid |= RequireDigit(datestring, 11);
+	invalid |= RequireDigit(datestring, 12);
+	
+	invalid |= RequireValue(datestring, 13, ':');
+	//minute
+	invalid |= RequireDigit(datestring, 14);
+	invalid |= RequireDigit(datestring, 15);
+
+	invalid |= RequireValue(datestring, 16, ':');
+	//second
+	invalid |= RequireDigit(datestring, 17);
+	invalid |= RequireDigit(datestring, 18);
+
+	if (str_length == 20)
+	{
+		invalid |= RequireValue(datestring, 19, 'Z');
+	}
+	if (str_length == 25)
+	{
+		char plusMinus = datestring[19];
+		if (plusMinus != '-' && plusMinus != "+")
+		{
+			invalid |= 1;
+		}
+		invalid |= RequireDigit(datestring, 20);
+		invalid |= RequireDigit(datestring, 21);
+		invalid |= RequireValue(datestring, 22, ':');
+		invalid |= RequireDigit(datestring, 23);
+		invalid |= RequireDigit(datestring, 24);
+	}
+
+
+
+	return invalid;
 }
 
 int parse_datetime(char* datestring, int str_length, struct DateTime* result)
--- BreakTime/TestBreakTime/TestBreakTime.cpp
+++ BreakTime/TestBreakTime/TestBreakTime.cpp
@@ -15,8 +15,68 @@ namespace TestBreakTime
 		TEST_METHOD(TestValidateDatetimeString)
 		{
 			char* ObviouslyGood = "2025-08-04T10:52:00-05:00";
-			int goodResult = validate_datetime_string_format(ObviouslyGood, sizeof(ObviouslyGood));
+			int goodResult = validate_datetime_string_format(ObviouslyGood, strlen(ObviouslyGood));
 			Assert::AreEqual(0, goodResult);
+			
+			char* UTCTz = "2025-08-04T10:52:00Z";
+			int UTCresult = validate_datetime_string_format(UTCTz, strlen(UTCTz));
+			Assert::AreEqual(0, UTCresult);
+
+
+			char* yearNotNumeric = "20A5-08-04T10:52:00-05:00";
+			int yearNotNumericResult = validate_datetime_string_format(yearNotNumeric, strlen(yearNotNumeric));
+			//for now all our invalid string error codes are 1
+			Assert::AreEqual(1, yearNotNumericResult);
+
+			char* monthNotNumeric = "2025-{8-04T10:52:00-05:00";
+			int monthNotNumericResult = validate_datetime_string_format(monthNotNumeric, strlen(monthNotNumeric));
+			//for now all our invalid string error codes are 1
+			Assert::AreEqual(1, monthNotNumericResult);
+
+			char* dayNotNumeric = "2025-08-0*T10:52:00-05:00";
+			int dayResult = validate_datetime_string_format(dayNotNumeric, strlen(dayNotNumeric));
+			//for now all our invalid string error codes are 1
+			Assert::AreEqual(1, dayResult);
+
+			char* hourNotNumeric = "2025-08-04T1P:52:00-05:00";
+			int hourResult = validate_datetime_string_format(hourNotNumeric, strlen(hourNotNumeric));
+			Assert::AreEqual(1, hourResult);
+
+			char* minuteNotNumeric = "2025-08-04T10:5X:00-05:00";
+			int minuteResult = validate_datetime_string_format(minuteNotNumeric, strlen(minuteNotNumeric));
+			Assert::AreEqual(1, minuteResult);
+
+			char* secondNotNumeric = "2025-08-04T10:52:0I-05:00";
+			int secondResult = validate_datetime_string_format(secondNotNumeric, strlen(secondNotNumeric));
+			Assert::AreEqual(1, secondResult);
+
+			char* TZHourNotNumeric = "2025-08-04T10:52:00-O5:00";
+			int tzHourResult = validate_datetime_string_format(TZHourNotNumeric, strlen(TZHourNotNumeric));
+			Assert::AreEqual(1, tzHourResult);
+
+			char* TZMinuteNotNumeric = "2025-08-04T10:52:00-05:^^";
+			int tzMinuteResult = validate_datetime_string_format(TZMinuteNotNumeric, strlen(TZMinuteNotNumeric));
+			Assert::AreEqual(1, tzMinuteResult);
+
+			char* wrongUTCLetter= "2025-08-04T10:52:00z"; //we think lower case is bad right?
+			int wrongUTCResult = validate_datetime_string_format(wrongUTCLetter, strlen(wrongUTCLetter));
+			Assert::AreEqual(1, wrongUTCResult);
+
+			char* switchColonsAndDashes= "2025:08:04T10-52-00:05-00";
+			int wrongDashes = validate_datetime_string_format(switchColonsAndDashes, strlen(switchColonsAndDashes));
+			Assert::AreEqual(1, wrongDashes);
+			
+			char* emptyString = "";
+			int emptyResult = validate_datetime_string_format(emptyString, strlen(emptyString));
+			Assert::AreEqual(1, emptyResult);
+			
+			char* twoGoodDatesInARow= "2025-08-04T10:52:00-05:002025-08-04T10:52:00-05:00";
+			int twoResult = validate_datetime_string_format(twoGoodDatesInARow, strlen(twoGoodDatesInARow));
+			Assert::AreEqual(1, twoResult);
+			
+			char* cutshort = "2025-08-04T10:52:0";
+			int cutResult = validate_datetime_string_format(cutshort, strlen(cutshort));
+			Assert::AreEqual(1, cutResult);
 		}
 	};
 }
